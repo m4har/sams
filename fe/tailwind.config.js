20db6508a45c6ff4b6ab99829f387298eff1a01e
Mahardicka <mahardicka404@gmail.com>
2024-11-20 19:32:43 +0700

    fe: add UI components for skeleton, separator, tooltip, and sheet; update package.json with new dependencies

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -3,41 +3,47 @@ export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {
-      colors: {
-        main: "#ff6b6b",
-        mainAccent: "#ff3333", // not needed for shadcn components
-        overlay: "rgba(0,0,0,0.8)", // background color overlay for alert dialogs, modals, etc.
-
-        // light mode
-        bg: "#fcd7d7",
-        text: "#000",
-        border: "#000",
-
-        // dark mode
-        darkBg: "#272933",
-        darkText: "#eeefe9",
-        darkBorder: "#000",
-        secondaryBlack: "#212121", // opposite of plain white, not used pitch black because borders and box-shadows are that color
-      },
-      borderRadius: {
-        base: "5px",
-      },
-      boxShadow: {
-        light: "4px 4px 0px 0px #000",
-        dark: "4px 4px 0px 0px #000",
-      },
-      translate: {
-        boxShadowX: "4px",
-        boxShadowY: "4px",
-        reverseBoxShadowX: "-4px",
-        reverseBoxShadowY: "-4px",
-      },
-      fontWeight: {
-        base: "500",
-        heading: "700",
-      },
-    },
+  	extend: {
+  		colors: {
+  			main: '#ff6b6b',
+  			mainAccent: '#ff3333',
+  			overlay: 'rgba(0,0,0,0.8)',
+  			bg: '#fcd7d7',
+  			text: '#000',
+  			border: '#000',
+  			darkBg: '#272933',
+  			darkText: '#eeefe9',
+  			darkBorder: '#000',
+  			secondaryBlack: '#212121',
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		},
+  		borderRadius: {
+  			base: '5px'
+  		},
+  		boxShadow: {
+  			light: '4px 4px 0px 0px #000',
+  			dark: '4px 4px 0px 0px #000'
+  		},
+  		translate: {
+  			boxShadowX: '4px',
+  			boxShadowY: '4px',
+  			reverseBoxShadowX: '-4px',
+  			reverseBoxShadowY: '-4px'
+  		},
+  		fontWeight: {
+  			base: '500',
+  			heading: '700'
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 };
